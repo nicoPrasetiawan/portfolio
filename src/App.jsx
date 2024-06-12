9e10c631ba92667b2d0ce31abe7d2fc4380ae360
nicoPrasetiawan <nicoprasetiawan28@gmail.com>
2024-06-12 14:10:31 +0700

    1. add default value on buttonContainer, 2. install react-toastify(but still unused)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./components/Header/Header.jsx";
 
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import Examples from "./components/Examples.jsx";
-import nico from "./assets/nico.png";
 
 {
   alert("This website is still under construction");
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,8 @@
-function Tabs({ children, buttons }) {
+function Tabs({ children, buttons, buttonsContainer = "menu" }) {
+  const ButtonsContainer = buttonsContainer;
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
